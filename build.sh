cb6b77c721ee916712ffcae4d124fad59e55c7da
root <root@vmi386732.contaboserver.net>
2024-03-27 23:34:13 +0600

    init

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo "Stopping pm2"
-sudo pm2 stop pai-business
-echo "pm2 stopped"
-
 echo "git pulling"
 git pull --rebase -v
 echo "git pulled"
@@ -11,6 +7,6 @@ echo "npm install"
 npm install
 echo "npm installed"
 
-echo "Starting pm2"
-pm2 restart pm2.config.cjs
-echo "pm2 started"
+echo "Build project started"
+npm run build
+echo "Project builded"
